c3875eb86370ac7990b961e18ddd980d367e6101
favour anaonye <favouranaonye@gmail.com>
2025-09-02 14:17:17 +0100

    Add actual project files

---
+++ app.js
@@ -0,0 +1,32 @@
+ const menuToggle = 
+  document.getElementById("menuToggle");
+const dropdownMenu = 
+  document.getElementById("dropdownMenu");
+
+// Toggle menu
+menuToggle.addEventListener("click", (e) => {
+  e.stopPropagation();
+  dropdownMenu.classList.toggle("show");
+});
+
+// Close menu when clicking outside
+document.addEventListener("click", function (e) {
+  if (!dropdownMenu.contains(e.target) && e.target !== menuToggle) {
+    dropdownMenu.classList.remove("show");
+  }
+});
+
+//why aggies secton
+
+function toggleFeature(box) {
+  const isActive = box.classList.contains("active");
+
+
+//close all other boxes in why choose us section
+
+document.querySelectorAll(".feature-box").forEach(b => b.classList.remove("active"))
+
+//cliked box
+
+if (!isActive) {box.classList.add("active");}
+}
